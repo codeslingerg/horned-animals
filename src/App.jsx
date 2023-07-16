01cdf22e4f9afa365ac37ba775d482f6257ef890
Gerard <gerard.g.johnson@gmail.com>
2023-07-16 06:59:58 -0500

    creating render portion

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,61 @@
+// Step 1 Make sure to import React
 import { useState } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+// Step 2 create a component
+render() {
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+    <Container>
+      <Header />
+      <Modal show={this.state.modalIsShowing} onHide={this.handleClose}>
+        <Modal.Header closeButton>
+          <Modal.Title>Animal</Modal.Title>
+        </Modal.Header>
+        <Modal.Body>
+          <h2>{this.state.modalHornedBeastName}</h2>
+          <img src={this.state.modalImgUrl} />
+        </Modal.Body>
+      
+      </Modal>
+      <Main handleOpenAnimal={this.handleOpenAnimal} />
+      <Footer />
+    </Container>
+  );
 }
 
+
+
+// function App() {
+//   const [count, setCount] = useState(0)
+
+//   return (
+//     <>
+//       <div>
+//         <a href="https://vitejs.dev" target="_blank">
+//           <img src={viteLogo} className="logo" alt="Vite logo" />
+//         </a>
+//         <a href="https://react.dev" target="_blank">
+//           <img src={reactLogo} className="logo react" alt="React logo" />
+//         </a>
+//       </div>
+//       <h1>Vite + React</h1>
+//       <div className="card">
+//         <button onClick={() => setCount((count) => count + 1)}>
+//           count is {count}
+//         </button>
+//         <p>
+//           Edit <code>src/App.jsx</code> and save to test HMR
+//         </p>
+//       </div>
+//       <p className="read-the-docs">
+//         Click on the Vite and React logos to learn more
+//       </p>
+//     </>
+//   )
+// }
+
+// Step 3 export your component
 export default App
