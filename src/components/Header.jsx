bcd9cf3d8ddf28a87a9740fc517bc3872c26f643
Gerard <gerard.g.johnson@gmail.com>
2023-07-20 21:02:23 -0500

    new changes for Lab 01

---
+++ src/components/Header.jsx
@@ -0,0 +1,16 @@
+// import React
+import React from "react";
+
+// Create component
+class Header extends React.Component {
+    render() {
+        return (
+            <div>
+                <h1>Collection of Beasts</h1>
+            </div>
+        );
+    }
+}
+
+// Export component
+export default Header;
